aa071883f1ed529f3080a4efb3609ddb88c74f93
ajmalMundumuzhi <muhajmal73@gmail.com>
2025-09-30 21:44:31 +0530

    added /api in api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = 'https://backend-jq71.onrender.com'
+const API_URL = 'https://backend-jq71.onrender.com/api'
 
 const api = axios.create({
     baseURL: API_URL,
